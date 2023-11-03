1c07925a1645933fb12ff71fb0dee7f68bd917ee
DianaWangui <dianawanguikt048@gmail.com>
2023-11-03 13:45:45 +0300

    A file that prints hello world

---
+++ C-programming/Tasks/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A fucntion that prints \"Hello, World\" message
+ * Return: 0 on Success execution
+ */
+int main(void)
+{
+	/* The printf() is used to print the message to stdout on the terminal */
+	printf("Hello, World\n");
+	return (0);
+}
