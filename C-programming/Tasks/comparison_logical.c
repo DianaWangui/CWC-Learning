a61ddb7ff700ba36e9fb72a5bbb2dd39e33f0cb8
DianaWangui <dianawanguikt048@gmail.com>
2023-11-25 18:27:17 +0300

    adding comments

--- C-programming/Tasks/comparison_logical.c
+++ C-programming/Tasks/comparison_logical.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+/**
+ * main - a main function that used logical operators
+ * to make the decision of the person who is the oldest
+ * or checking if ages are equal
+ * Return: 0 on success
+ */
+
 int main(void)
 {
+	/* declaring the persons ages that will be used to store their ages */
 	int age_person1, age_person2, age_person3;
 
 	printf("A program that simulates decision making using comparison and logical operators\n");
 
+	/*Taking each persons age from the user*/
 	printf("Enter the age of the first person: ");
 	scanf("%d", &age_person1);
 
@@ -14,6 +23,12 @@ int main(void)
 	printf("\nEnter the age of the third person: ");
 	scanf("%d", &age_person3);
 
+	/**
+	 * Comparing the three ages using logical and (&&) 
+	 * operator to decide the oldest
+	 * or using logical or(||) operator to check if there 
+	 * is any more than two ages which are equal
+	 */
 	if (age_person1 > age_person2 && age_person1 > age_person3)
 	{
 		printf("\nPerson 1 is the oldest.");
