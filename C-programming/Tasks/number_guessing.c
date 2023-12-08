77f3016bca63ac444aff4bcef86ce5d48641d425
DianaWangui <dianawanguikt048@gmail.com>
2023-12-08 22:30:04 +0300

    more code and use of break and continue

--- C-programming/Tasks/number_guessing.c
+++ C-programming/Tasks/number_guessing.c
@@ -9,29 +9,46 @@ int main(void)
   /*srand() is a function in c used to generate random numbers */
   /* I used srand(time(NULL)) to randomly chose
    a different number when the program is run*/
-  // srand(time(NULL));
+  srand(time(NULL));
 
-  // number = rand() % 100;
   number = rand() % 100;
-  // printf("Number: %d\n", number);
-  while (number > 0)
+  /* We use while loop to iterate */
+  while (1)
   {
     printf("enter your guess (1-100): ");
     scanf("%d", &user_no);
+    /*Checking if user input is beyond the targetted input range
+     *the loop will continue until the user input is in range 1-100
+     */
+    if (user_no < 0 || user_no > 100)
+    {
+      printf("Please enter a positive number\n");
+      continue;
+    }
+    /* Count is used here to count number of time user guessed and its incremented each time user guesses */
     count++;
 
+    /* if user guess==random generated no congratulates, return the number of counts and break out of the loop */
     if (user_no == number)
     {
       printf("Congratulations! You guessed the correct number in %d attempts.", count);
       break;
     }
-    if (user_no < number)
+    /* if number is less print the try higher hint and continue*/
+    else if (user_no < number)
     {
       printf("Incorrect! Try higher.\n");
     }
-    if (user_no > number)
+    /* if user_no is greater than random number print the try lower hint and continue*/
+    else if (user_no > number)
     {
       printf("Incorrect! Try lower.\n");
     }
+    /* I dont think this will get executed, but I think it should handle non-int value*/
+    else
+    {
+      printf("Incorrect! Try again.\n");
+    }
   }
+  return (0);
 }
