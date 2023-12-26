26aba4105f1e8265edeb8328f2dac8909b7a4c19
DianaWangui <dianawanguikt048@gmail.com>
2023-12-26 14:50:04 +0300

    finally a simple calculator

--- C-programming/Tasks/functions/math_operations.c
+++ C-programming/Tasks/functions/math_operations.c
@@ -3,48 +3,49 @@ int addition(int num1, int num2);
 int subtract(int num1, int num2);
 int multiplication(int num1, int num2);
 int division(int num1, int num2);
-int calculate(int num1, int num2);
+int calculate(int num1, int num2, int (*func_ptr[4])(int, int));
 
 int main(void)
 {
   int number1, number2;
+  int (*func_ptr[4])(int, int);
   int repeat;
-  int result;
-
-  result = calculate(number1, number2);
-  printf("Result is %d", result);
-
-  printf("\nDo you want to perform another calculation? (1 for Yes and 0 for No): ");
-  scanf("%d", &repeat);
-
-  while (repeat == 1)
+  do
   {
-    calculate(number1, number2);
-  }
+    printf("Enter the first number: ");
+    scanf("%d", &number1);
+    printf("Enter second number: ");
+    scanf("%d", &number2);
 
-  return (0);
+    calculate(number1, number2, func_ptr);
+
+    printf("\nDo you want to perform another calculation? (1 for Yes and 0 for No): ");
+    scanf("%d", &repeat);
+  } while (repeat == 1);
+  return 0;
 }
 
-int calculate(int num1, int num2)
+int calculate(int num1, int num2, int (*func_ptr[4])(int, int))
 {
-  int (*func_ptr[])(int, int) = {addition, subtract, multiplication, division};
-  int choice;
-  int result;
-  int repeat;
+  int choice, result;
+  func_ptr[0] = addition;
+  func_ptr[1] = subtract;
+  func_ptr[2] = multiplication;
+  func_ptr[3] = division;
 
-  printf("Enter the first number: ");
-  scanf("%d", &num1);
-  printf("Enter second number: ");
-  scanf("%d", &num2);
-
-  printf("Select Operation:\n0. Addition\n1. Subtraction\n2. Multiplication\n3. Division\nEnter the operation: ");
+  printf("Select Operation:\n0. Addition\n1. Subtraction\n2. Multiplication\n3. Division\n");
+  printf("Enter the operaion: ");
   scanf("%d", &choice);
-  while (choice <= 3 && choice >= 0)
+  if (choice >= 0 && choice <= 3)
   {
-    result = (*func_ptr[choice])(num1, num2);
-    break;
+    result = func_ptr[choice](num1, num2);
+    printf("Result: %d", result);
   }
-  return result;
+  else
+  {
+    printf("Invalid operation.\n");
+  }
+  return func_ptr[choice](num1, num2);
 }
 
 /*Additional function*/
